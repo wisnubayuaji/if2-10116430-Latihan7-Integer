7ae0c15f6ca20505348b3981e3ba5b6a0a9cc60e
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:41:31 +0700

    Add files via upload

---
+++ if2-10116430-Latihan7-Integer/src/App.java
@@ -0,0 +1,19 @@
+/**
+ * @author Nim : 10116430 Nama : Wisnu Bayu Aji Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+    }
+}
